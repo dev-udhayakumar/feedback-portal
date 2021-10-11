1e953f81553ab4f8018adc45de3946e06b198bc5
Udhayakumar T <60465112+dev-udhayakumar@users.noreply.github.com>
2021-10-11 18:37:00 +0530

    2

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,6 +14,7 @@ function Home(){
         </div><div>
             <center>
                 <h1 >Hello! Udhayakumar</h1>
+                <h2>vanakkam</h2>
                 </center>
             </div></>
                 
