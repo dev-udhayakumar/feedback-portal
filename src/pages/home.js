016fba9f4f322a36129ad91857267c8c06544a9c
Udhayakumar T <60465112+dev-udhayakumar@users.noreply.github.com>
2021-10-14 11:06:31 +0530

    1st update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Button,Container,Navbar} from 'react-bootstrap';
+import { getAuth, signOut } from "firebase/auth";
+import { useHistory } from "react-router-dom";
 
 function Home(){
+    const history = useHistory();
+    function signout(){
+        const auth = getAuth();
+        signOut(auth).then(() => {
+          // Sign-out successful.
+          history.push("/Login");
+        }).catch((error) => {
+          // An error happened.
+          alert(error)
+          console.log(error);
+        });
+    }
     return(
         <><div>
             <Container fluid="true">
                 <Navbar expand="lg" variant="light" bg="light">
-                    <Button className="m-1" variant="outline-primary" href="/">
+                    <Button className="m-1" variant="outline-primary" onClick={signout}>
                         Logout
                     </Button>
                 </Navbar>
             </Container>
         </div><div>
             <center>
-                <h1 >Hello! Udhayakumar</h1>
-                <h2>vanakkam</h2>
+                <h1 >u r logged in successfully</h1>
                 </center>
             </div></>
                 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,52 +1,65 @@
 import React from "react";
-import { Button , Card ,Container ,Form,Navbar} from 'react-bootstrap';
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useHistory } from "react-router-dom";
 
 const Signup = () =>{
+    const [email,setemail] = useState('');
+    const [password,setpassword] = useState('');
+    const [ loading, setLoading ] = useState(false);
+    const history = useHistory();
+    function signin(){
+        setLoading(true)
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            history.push("/Login");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert("Invalid email or password")
+            console.log(errorCode , errorMessage)
+        }).finally(() => setLoading(false));;
+    }
+    function alreadyhaveaccounnt(){
+        history.push("/Login")
+    }
+
     return(
-        <><div>
-            <Container fluid="true">
-                <Navbar expand="lg" variant="light" bg="light">
-                    <Container fluid="xxl">
-                        <Navbar.Brand href="#">React-firebase</Navbar.Brand>
-                    </Container>
-                </Navbar>
-            </Container>
-        </div><Container>
-                <div className="d-flex justify-content-center mt-5">
-                    <Card  border="light" style={{ width: '22rem' }}>
-                        <Card.Body>
-                            <Card.Title className="mb-4" style={{fontSize:"30px"}}>Create An Account</Card.Title>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control type="name" placeholder="Full name" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Set Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <div className="d-grid gap-2 mt-3">
-                            <Button variant="primary" >
-                                Signup
-                            </Button>
-                            </div>
-                            <div className="d-grid gap-2 mt-3">
-                            <Button variant="outline-primary" href="/" >
-                                Already have an Account
-                            </Button>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </div>
-            </Container></>
-        
+        <div >
+            <div >
+            <nav className="navbar navbar-dark bg-primary">
+            <h3 className="ms-3">Feedback-portal</h3>
+                </nav>
+            </div>
+            <div className="d-flex justify-content-center mt-4">
+            <div className="card shadow border-light rounded" Style="width: 22rem;">
+            <h2 className="card-title m-3">Create Account</h2>
+            <div className="card-body">
+            <div className="mb-3">
+                <label className="form-label">Email address</label>
+                <input type="email" className="form-control" Value={email} onInput={e => setemail(e.target.value)} placeholder="Enter email"  />
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input type="password" className="form-control" Value={password} onInput={e => setpassword(e.target.value)} placeholder="Password" />
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Confirm Password</label>
+                <input type="password" className="form-control" placeholder="Confirm Password" />
+            </div>
+            <div className="d-grid gap-2 mt-4">
+            <button className="btn btn-primary" type="button" onClick={signin}>{loading ? 'Loading..' : 'Signup'}</button>
+            <button className="btn btn-outline-primary"  onClick={alreadyhaveaccounnt}>Already Have an Account</button>
+            </div>
+            </div>
+            </div>
+            </div> 
+        </div>
     )
 }
 
