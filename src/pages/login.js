fcb980deae20b85f439d046f7c5b19b5f027dab0
Udhayakumar T <60465112+dev-udhayakumar@users.noreply.github.com>
2021-10-21 12:37:46 +0530

    seritha

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,7 +11,6 @@ const Login= () =>{
     const [ loading, setLoading ] = useState(false);
     const history = useHistory();
     const auth = getAuth();
-    const user = auth.currentUser;
     if(currentUser){
         return <Redirect to="/home" />
     }
